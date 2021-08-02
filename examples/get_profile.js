05d9770ebed23e844d37c5cf47b7fe52e0b4ac74
joshp-f <jporrittfraser@gmail.com>
2021-08-02 17:53:13 +1000

    fix

--- examples/get_profile.js
+++ examples/get_profile.js
@@ -5,4 +5,4 @@ gs(url, function(err, data) {
 
   fs.writeFileSync(__dirname + "/" + url + ".json", JSON.stringify(data, null, 2))
   console.log(data); // or what ever you want to do with the data
-})
+},{domain:'webcache.googleusercontent.com',port:443,pathFn:(path) => `/search?q=cache:https://github.com${path}`})
